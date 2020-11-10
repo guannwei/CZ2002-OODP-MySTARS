0349cae49c4d1de9fd14f0389178ba0bab349eb8
guannwei <guanwei58@hotmail.com>
2020-11-10 22:42:33 +0800

    Moved validation to UI

--- MySTARS/src/Boundary/AdminUI.java
+++ MySTARS/src/Boundary/AdminUI.java
@@ -1,15 +1,26 @@
 package Boundary;
 
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import Controller.StudentController;
 import Model.*;
 
 public class AdminUI {
+	
+	public static StudentController stuCtrl = new StudentController();
+	
 	public static void adminMenu(Admin admin) {
 		int choice = 0;
 		boolean validInput = false;
+		String matric = "";
 		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
 		do {
 			System.out.println("1. Edit student access period");
@@ -37,8 +48,101 @@ public class AdminUI {
 
 			switch (choice) {
 			case 1:
+				validInput = false;
+				LocalDateTime start = LocalDateTime.now();
+				LocalDateTime end = LocalDateTime.now();
+				do {
+					System.out.println("Enter student's matric number: ");
+					matric = sc.nextLine();
+					if(stuCtrl.checkMatricExists(matric) == true){
+						validInput = true;
+					}
+					else {
+						System.out.println("Student does not exist! Please enter a valid matric number!");
+					}
+				}
+				while(!validInput);
+				
+				
+				
+				do {
+					try {
+						System.out.println("Enter start date time for access(YYYY-MM-DD HH:MM): ");
+						start = LocalDateTime.parse(sc.nextLine(), formatter);
+						validInput = true;
+					}
+					catch(DateTimeParseException e) {
+						System.out.println("Please enter the valid date time format!");
+					}
+				}while(!validInput);
+				
+				validInput = false;
+				
+				do {
+					try {
+						System.out.println("Enter end date time for access(YYYY-MM-DD HH:MM): ");
+						end = LocalDateTime.parse(sc.nextLine(), formatter);
+						validInput = true;
+					}
+					catch(DateTimeParseException e) {
+						System.out.println("Please enter the valid date time format!");
+					}
+					
+				}while(!validInput);
+				
+
+				stuCtrl.editAccessPeriod(matric, start, end);
+				
 				break;
 			case 2:
+				System.out.println("Enter student's matric number: ");
+				matric = sc.nextLine();
+				System.out.println("Enter student's name: ");
+				String name = sc.nextLine();
+				System.out.println("Enter student's username: ");
+				String username = sc.nextLine();
+				System.out.println("Enter student's password: ");
+				String password = sc.nextLine();
+				System.out.println("Enter student's email: ");
+				String email = sc.nextLine();
+				System.out.println("Enter student's gender: (Male/Female)");
+				String gender = sc.nextLine();
+				System.out.println("Enter student's nationality: ");
+				String nationality = sc.nextLine();
+				
+				Student stu = new Student(username, password, email, name, matric, gender, nationality);
+				
+				//Check if student exists
+				if(stuCtrl.checkStudentExists(stu) == true) {
+					System.out.println("Student already exists! Please enter fields again!");
+				}
+				else{
+					//Check if matric already exists
+					if(stuCtrl.checkMatricExists(matric) == true) {
+						System.out.println("Matric number already exists! Please enter fields again!");
+					}
+					else {
+						if(stuCtrl.checkUserNameExists(username) == true) {
+							System.out.println("Username already exists! Please enter fields again!");
+						}
+						else {
+							//Check if email exists
+							if(stuCtrl.checkEmailExists(email) == true) {
+								System.out.println("Email already exists! Please enter fields again!");
+							}
+							else {
+								//Check if gender was entered correctly
+								if(!gender.equals("Female") || !gender.equals("Male")) {
+									System.out.println("Please enter correct format for gender(Male/Female)! Please enter fields again!");
+								}
+								else {
+									stuCtrl.addStudent(stu);
+								}
+							}
+						}
+					}
+				}
+
 				break;
 			case 3:
 
--- MySTARS/src/Controller/StudentController.java
+++ MySTARS/src/Controller/StudentController.java
@@ -28,6 +28,22 @@ public class StudentController {
 		return exists;
 	}
 	
+	public Boolean checkUserNameExists(String username){
+		Boolean exists = false;
+		try {
+			ArrayList<Student> studentList = accessFile.readStudents();
+			for(int i = 0; i<studentList.size(); i++) {
+				if(studentList.get(i).getUsername().equals(username)) {
+					exists = true;
+					break;
+				}	
+			}
+		}catch(Exception e) {
+			
+		}
+		return exists;
+	}
+	
 	public Boolean checkStudentExists(Student stu){
 		Boolean exists = false;
 		try {
@@ -65,66 +81,34 @@ public class StudentController {
 		try {
 			//Get list of students
 			ArrayList<Student> studentList = accessFile.readStudents();
-			//Check if student exists
-			if(checkMatricExists(matric) == true) {
-				//Edit student
-				for(int i = 0; i<studentList.size(); i++) {
-					if(studentList.get(i).getMatricNumber().equals(matric)) {
-						studentList.get(i).setAccessStartPeriod(start);
-						studentList.get(i).setAccessEndPeriod(end);
-						break;
-					}
+			//Edit student
+			for(int i = 0; i<studentList.size(); i++) {
+				if(studentList.get(i).getMatricNumber().equals(matric)) {
+					studentList.get(i).setAccessStartPeriod(start);
+					studentList.get(i).setAccessEndPeriod(end);
+					break;
 				}
-				//Write back to file
-				accessFile.saveStudent(studentList);
-				System.out.println("Access period successfully edited!");
 			}
-			else {
-				System.out.println("Student does not exist!");
-			}
-			
+			//Write back to file
+			accessFile.saveStudent(studentList);
+			System.out.println("Access period successfully edited!");
 		}
 		catch(Exception e){
-			
 		}
-		
 	}
 	
 	public void addStudent(Student stu){
 		try {
 		//Get list of students
 		ArrayList<Student> studentList = accessFile.readStudents();
-		if(stu.getMatricNumber().length() >= 8) {
-			if(checkStudentExists(stu) == false) {
-				if(checkMatricExists(stu.getMatricNumber()) == false) {
-					if(checkEmailExists(stu.getEmail()) == false) {
-						//Add student
-						studentList.add(stu);	
-						//Write back to file
-						accessFile.saveStudent(studentList);
-						System.out.println("Student successfully added!");	
-					}
-					else {
-						System.out.println("Email already exists! Please enter fields again!");
-					}
-					
-				}
-				else {
-					System.out.println("Matric number already exists! Please enter fields again!");
-				}
-				
-			}
-			else {
-				System.out.println("Student already exists! Please enter fields again!");
-			}
-			
-		}
-		else {
-			System.out.println("Matric number is too short. It needs to be at least 7 letters. Please enter fields again!");
-		}
-		
-			
-		
+
+		//Add student
+		Student stuAccess = new Student(stu.getUsername(), stu.getPassword(), stu.getEmail(), stu.getName(), stu.getMatricNumber(), stu.getGender(), stu.getNationality(), LocalDateTime.now(), LocalDateTime.now());
+		studentList.add(stuAccess);
+		//Write back to file
+		accessFile.saveStudent(studentList);
+		System.out.println("Student successfully added!");	
+
 		}
 		catch(Exception e) {
 		
